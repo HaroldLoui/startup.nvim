ddda1d691b012125be4b7bcd3e9a7748bc0512d7
HaroldLoui <63445267+HaroldLoui@users.noreply.github.com>
2023-03-26 23:02:25 +0800

    修改键位

--- lua/startup/themes/dashboard.lua
+++ lua/startup/themes/dashboard.lua
@@ -29,8 +29,8 @@ local settings = {
         margin = 5,
         content = {
             { " Find File", "Telescope find_files", "<leader>ff" },
-            { " Find Word", "Telescope live_grep", "<leader>lg" },
-            { " Recent Files", "Telescope oldfiles", "<leader>of" },
+            { " Find Word", "Telescope live_grep", "<leader>fg" },
+            { " Recent Files", "Telescope oldfiles", "<leader>fo" },
             { " File Browser", "Telescope file_browser", "<leader>fb" },
             { " Colorschemes", "Telescope colorscheme", "<leader>cs" },
             { " New File", "lua require'startup'.new_file()", "<leader>nf" },
